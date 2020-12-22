de38c2d1bc401269ba92ff1e8c6d264cf49c2301
Lukas Jury <Lukas.jury13@gmail.com>
2020-12-22 17:52:53 +1300

    added sounds

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
 import './App.css';
+import { Button } from 'react-bootstrap'
+import poopooigloo2 from './poopooigloo2.wav';
+import { useEffect, useState } from 'react'
 
 function App() {
+  let ppi = new Audio(poopooigloo2)
+  const [trackTime, setTrackTime] = useState(0);
+
+
+  const playSound = () => {
+    ppi.currentTime = trackTime
+    ppi.play()
+  }
+
+  const pauseSound = () => {
+    ppi.pause()
+    setTrackTime(ppi.currentTime)
+  }
+
+  const stopSound = () => {
+    ppi.pause()
+    ppi.currentTime = 0;
+  }
+
+  useEffect(() => {
+    
+    
+
+  }, [trackTime]);
+
   return (
-   <div >
-      <h1>Wassup</h1>
-   </div>
+    <div >
+      <Button onClick={playSound}>Play</Button>
+      <Button onClick={pauseSound}>Pause</Button>
+      <Button onClick={stopSound}>Stop</Button>
+
+      <input id="musicSlider" type="range" min="0" max={ppi.duration} value={trackTime} class="slider" />
+    </div>
+
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Header from './Header'
 import reportWebVitals from './reportWebVitals';
-
+import "bootstrap/dist/css/bootstrap.css";
 ReactDOM.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
