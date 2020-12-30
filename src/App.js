47a704f55b40eb30b69100187fde426ea01cefc5
lukas <lukas.jury13@gmail.com>
2020-12-30 18:12:57 +1300

    from mac

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
-import './App.css';
-import { Button } from 'react-bootstrap'
-import poopooigloo2 from './poopooigloo2.wav';
 import { useEffect, useState } from 'react'
+i
+
 
 function App() {
-  let ppi = new Audio(poopooigloo2)
-  const [trackTime, setTrackTime] = useState(0);
 
 
-  const playSound = () => {
-    ppi.currentTime = trackTime
-    ppi.play()
-  }
 
-  const pauseSound = () => {
-    ppi.pause()
-    setTrackTime(ppi.currentTime)
-  }
-
-  const stopSound = () => {
-    ppi.pause()
-    ppi.currentTime = 0;
-  }
-
-  useEffect(() => {
-    
-    
-
-  }, [trackTime]);
 
   return (
     <div >
-      <Button onClick={playSound}>Play</Button>
-      <Button onClick={pauseSound}>Pause</Button>
-      <Button onClick={stopSound}>Stop</Button>
-
-      <input id="musicSlider" type="range" min="0" max={ppi.duration} value={trackTime} class="slider" />
+        <h1>hi</h1>
     </div>
 
   )
